a15500b6e3d45d58149c59f6fac87c3c589982da
Adam <adamskoullos@gmail.com>
2022-01-03 16:45:40 +0000

    Add news card link to dynamic page

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -11,34 +11,37 @@ import {
 } from "@mui/material";
 import { Box } from "@mui/system";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Link } from "react-router-dom";
 
-const NewsCard = () => {
+const NewsCard = ({ url }) => {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
-      <Card variant="outlined">
-        <CardActionArea>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: "goldenRod" }} aria-label="recipe">
-                V
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="The news event headline"
-            subheader="September 14, 2016"
-          />
-          <CardMedia
-            component="img"
-            height="150rem"
-            image="https://media.timeout.com/images/105124791/750/422/image.jpg"
-            alt="Paella dish"
-          />
-        </CardActionArea>
-      </Card>
+      <Link to={url} style={{ textDecoration: "none" }}>
+        <Card variant="outlined">
+          <CardActionArea>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: "goldenRod" }} aria-label="recipe">
+                  V
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title="The news event headline"
+              subheader="September 14, 2016"
+            />
+            <CardMedia
+              component="img"
+              height="150rem"
+              image="https://media.timeout.com/images/105124791/750/422/image.jpg"
+              alt="Paella dish"
+            />
+          </CardActionArea>
+        </Card>
+      </Link>
     </Grid>
   );
 };
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,26 +3,19 @@ import { Link } from "react-router-dom";
 import NewsCard from "../components/NewsCard";
 
 const News = () => {
+  const url = "/news/1";
+
   return (
     <div style={{ height: "100%" }}>
       <Typography variant="h1" color="primary" sx={{ pt: "5vh" }}>
         News Page
       </Typography>
-      <Link
-        to="/news/1"
-        style={{
-          color: "goldenRod",
-          textDecoration: "none",
-        }}
-      >
-        News Item
-      </Link>
       <Grid container spacing={4} sx={{ mt: 3 }}>
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+        <NewsCard url={url} />
+        <NewsCard url={url} />
+        <NewsCard url={url} />
+        <NewsCard url={url} />
+        <NewsCard url={url} />
       </Grid>
     </div>
   );
