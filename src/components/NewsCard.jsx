79d75a44474906fa6bef618ef2fa038623ffee6f
Adam <adamskoullos@gmail.com>
2022-01-03 16:28:33 +0000

    Add cardactionarea to news card

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,11 +1,12 @@
 import {
   Avatar,
   Card,
+  CardActionArea,
+  CardActions,
   CardHeader,
   CardMedia,
   Grid,
   IconButton,
-  Paper,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
@@ -15,38 +16,29 @@ const NewsCard = () => {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <Card variant="outlined">
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "goldenRod" }} aria-label="recipe">
-              V
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="The news event headline"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          component="img"
-          height="150rem"
-          image="https://media.timeout.com/images/105124791/750/422/image.jpg"
-          alt="Paella dish"
-        />
-      </Card>
-      {/* <Paper elevation={3} sx={{ overflow: "hidden" }}>
-        <div className="card-img">
-          <img
-            src="https://media.timeout.com/images/105124791/750/422/image.jpg"
-            alt="test"
+        <CardActionArea>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: "goldenRod" }} aria-label="recipe">
+                V
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="The news event headline"
+            subheader="September 14, 2016"
           />
-        </div>
-        <Box p={2}>
-          <Typography>Title</Typography>
-        </Box>
-      </Paper> */}
+          <CardMedia
+            component="img"
+            height="150rem"
+            image="https://media.timeout.com/images/105124791/750/422/image.jpg"
+            alt="Paella dish"
+          />
+        </CardActionArea>
+      </Card>
     </Grid>
   );
 };
